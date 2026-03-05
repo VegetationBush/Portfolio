fa37415be18d27f24f002c6843005159d92fa1f2
Hans Yang <hans.yangchenghao@gmail.com>
2026-03-04 21:35:32 -0500

    perspective

--- src/components/PerspectiveCard.tsx
+++ src/components/PerspectiveCard.tsx
@@ -8,30 +8,36 @@ interface PerspectiveCardProps {
   height?: number; // optional, default 400
 }
 
+const PERSPECTIVE_CARD_STRENGTH = 0.1
 const PerspectiveCard: React.FC<PerspectiveCardProps> = ({
   title = "",
   description = "",
-  width = 50,
-  height = 50,
+  width = 350,
+  height = 350,
 }) => {
   const cardRef = useRef<HTMLDivElement>(null);
 
   const handleMouseMove = (e: MouseEvent<HTMLDivElement>) => {
     if (!cardRef.current) return;
 
+    // retrieving values
     const card = cardRef.current;
     const rect = card.getBoundingClientRect();
-    const x = e.clientX - rect.left;
-    const y = e.clientY - rect.top;
+    const mouseX = e.clientX - rect.left;
+    const mouseY = e.clientY - rect.top;
+    const cardCenterX = rect.left + rect.width / 2;
+    const cardCenterY = rect.top + rect.height / 2;
 
-    const centerX = rect.width / 2;
-    const centerY = rect.height / 2;
+    const angle = Math.atan2(mouseY - cardCenterY, mouseX - cardCenterX)
+    const percentX = Math.abs((mouseX - cardCenterX) / cardCenterX); // percent distance from the center and outer x bound
+    const percentY = Math.abs((mouseY - cardCenterY) / cardCenterY); // percent distance from the center and outer y bound
 
-    const rotateX = 25; // max tilt
-    const rotateY = -25;
-
-    card.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
-    card.style.boxShadow = `${-rotateY * 2}px ${rotateX * 2}px 40px rgba(0,0,0,0.05)`;
+    // percentY is used on rotateX and vice versa since their axis rotates each other
+    const rotateX = Math.sin(angle) * -PERSPECTIVE_CARD_STRENGTH * percentY;
+    const rotateY = Math.cos(angle) * PERSPECTIVE_CARD_STRENGTH * percentX;
+    
+    card.style.transform = `rotateX(${rotateX}rad) rotateY(${rotateY}rad)`;
+    // card.style.boxShadow = `5px 5px 20px 20px black`;
   };
 
   const handleMouseLeave = () => {
@@ -39,18 +45,23 @@ const PerspectiveCard: React.FC<PerspectiveCardProps> = ({
 
     const card = cardRef.current;
     card.style.transform = 'rotateX(0deg) rotateY(0deg)';
-    card.style.boxShadow = '0 20px 40px rgba(0,0,0,0.05)';
+    // card.style.boxShadow = '5px 5px 20px 20px black';
   };
 
   return (
     <div
       className="interactive-card-container"
-      style={{ width, height , backgroundColor: "gray", position: "absolute"}}
+      style={{
+        width,
+        height,
+        position: "absolute",
+        perspective: "1500px",
+      }}
       onMouseMove={handleMouseMove}
       onMouseLeave={handleMouseLeave}
     >
-      <div className="interactive-card" ref={cardRef}>
-        <h2>{title}</h2>
+      <div className="interactive-card" ref={cardRef} style = {{position: "absolute", height: "350px", width: "350px", color: "white"}}>
+        <h2 style = {{backgroundColor: "black", height: "100%", borderRadius: 25, boxShadow: "5px 5px 25px 10px black"}}>{title + "lorem ipsum sor dit amet"}</h2>
         <p>{description}</p>
       </div>
     </div>
