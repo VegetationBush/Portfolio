d67c82af467d84cc0db7a3df6fce31ec6d28e06e
Hans Yang <hans.yangchenghao@gmail.com>
2026-04-29 17:01:00 -0400

    bruh

--- src/App.tsx
+++ src/App.tsx
@@ -29,11 +29,11 @@ function App() {
   return (
     <div style = {{
       display: "flex",
-      height: "100vh",
+      height: "100dvh",
       width: "100vw",
       flexDirection: "column",
       backgroundColor: "var(--background)",
-      overflow: "clip",
+      // overflow: "clip",
     }}>
       <FadeInObserver/>
       <Navbar/>
