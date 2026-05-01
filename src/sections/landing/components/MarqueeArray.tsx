0546393bc22c41e1d533863772a0215597901105
Hans Yang <hans.yangchenghao@gmail.com>
2026-05-01 13:41:04 -0400

    marquee polish

--- src/sections/landing/components/MarqueeArray.tsx
+++ src/sections/landing/components/MarqueeArray.tsx
@@ -1,3 +1,5 @@
+import "./marqueeArray.css"
+
 import type { ReactElement } from "react";
 import Marquee from "./Marquee";
 
@@ -7,18 +9,16 @@ const images = import.meta.glob<string>('/src/sections/landing/components/marque
 });
 const itemList = Object.values(images);
 
-const ELEMENT_SIZE_REM = 11
 const listToElements = (list: string[]): ReactElement[] => {
   return list.map((item, i) => {
     return <img
-      key={i}
-      src={item}
-      style={{
-        width: `${ELEMENT_SIZE_REM}rem`,
-        height: `${ELEMENT_SIZE_REM}rem`,
+      key = {i}
+      src = {item}
+      className = "marquee-array-item"
+      style = {{
         objectFit: "cover",
         marginRight: "1rem",
-        borderRadius: "1.5rem",
+        borderRadius: "12.5%",
       }}
     />
   })
@@ -44,26 +44,26 @@ const MarqueeArray = () => {
           animation: "marquee-array-fade 3s ease-in forwards",
         }}
       >
-        <div style = {{
-          height: "100%",
-          width: "100%",
+        <div
+          className = "marquee-array-container"
+          style = {{
+            height: "100%",
+            width: "100%",
 
-          display: "flex",
-          flexDirection: "column",
-          gap: "1rem",
-          
-          transform: "rotate(-35deg)",
-          transformOrigin: "top left",
-        }}>
+            display: "flex",
+            flexDirection: "column",
+            
+            transform: "rotate(-35deg)",
+            transformOrigin: "top left",
+          }}
+        >
           {
             Array.from({ length: 7 }, (_, i) => (
               <div
                 key = {i}
                 style = {{
                   position: "relative",
-
-                  // since the container is rotated 35 degrees from the top left, each element must be adjusted so aligns vertically, leaving no gaps
-                  right: `${ELEMENT_SIZE_REM * (i + 1)}rem`,
+                  right: `calc(var(--marquee-item-size) * ${i-1})`,
                 }}
               >
                 <Marquee items = {listToElements(itemList)} duration = {10 + i * 3}/>
